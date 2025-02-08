fb00445854a3915cbd03f882916f12da5a05be68
max <max.assis@chatguru.com.br>
2025-02-08 16:13:54 -0300

    feat: feito a validação do cart feito a validação do cart

--- src/controllers/cart_controller.ts
+++ src/controllers/cart_controller.ts
@@ -1,13 +1,32 @@
 import { Request, Response } from "express";
+import { CartSchema } from "../schemas/cart_schema"; // Importa o schema Zod
+import { ZodError } from "zod"; // Importa o tipo ZodError
 import CartService from "../services/cart_service";
 
 export const updateOrCreateCart = async (req: Request, res: Response) => {
   try {
-    const { userId, items } = req.body;
+    const validatedData = CartSchema.parse(req.body);
+
+    const { userId, items } = validatedData;
 
     const cart = await CartService.updateOrCreateCart(userId, items);
-    res.status(200).json(cart);
+
+    return res
+      .status(200)
+      .json({ message: "Carrinho atualizado com sucesso", data: cart });
   } catch (error) {
-    res.status(500).json({ message: "Erro ao atualizar ou criar carrinho" });
+    if (error instanceof ZodError) {
+      return res.status(400).json({
+        message: "Erro de validação dos dados",
+        errors: error.errors.map((err) => ({
+          campo: err.path.join("."),
+          mensagem: err.message,
+        })),
+      });
+    }
+
+    return res
+      .status(500)
+      .json({ message: "Erro interno ao processar o carrinho" });
   }
 };
