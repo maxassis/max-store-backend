42482d82621f72f41a88d35ba7edb20474e6db89
max <max.assis@chatguru.com.br>
2025-02-09 14:45:01 -0300

    feat: adicionado node-cache para cache de produtos

--- src/services/product_service.ts
+++ src/services/product_service.ts
@@ -1,3 +1,4 @@
+import NodeCache from "node-cache";
 import Produto from "../models/product";
 
 export interface IProdutoInput {
@@ -8,28 +9,61 @@ export interface IProdutoInput {
   stock: number;
 }
 
+const cache = new NodeCache({ stdTTL: 600 });
+
 class ProdutoService {
   // Listar todos os produtos em estoque
   async listarProdutos() {
+    const cacheKey = "lista_produtos";
+
     try {
+      const cachedData = cache.get(cacheKey);
+      if (cachedData) {
+        // console.log("Retornando lista de produtos do cache");
+        return cachedData;
+      }
+
+      // console.log("Buscando lista de produtos no banco de dados...");
+
       const produtos = await Produto.find({ stock: { $gt: 0 } });
+
+      cache.set(cacheKey, produtos);
+
       return produtos;
     } catch (error) {
       throw new Error("Erro ao listar produtos");
     }
   }
 
-  // retornar um unico produto
+  // Retornar um único produto
   async obterProduto(id: string) {
+    const cacheKey = `produto_${id}`;
+
     try {
+      // Verifica se o produto já está em cache
+      const cachedData = cache.get(cacheKey);
+      if (cachedData) {
+        // console.log(`Retornando produto ${id} do cache`);
+        return cachedData;
+      }
+
+      // console.log(`Buscando produto ${id} no banco de dados...`);
+
       const produto = await Produto.findById(id);
+
+      if (!produto) {
+        throw new Error("Produto não encontrado");
+      }
+
+      cache.set(cacheKey, produto);
+
       return produto;
     } catch (error) {
       throw new Error("Erro ao obter produto");
     }
   }
 
-  //  Criar um novo produto
+  // Criar um novo produto
   async criarProduto(dados: IProdutoInput) {
     try {
       const { name, description, image, price, stock } = dados;
@@ -42,6 +76,10 @@ class ProdutoService {
         stock,
       });
       await novoProduto.save();
+
+      // Limpa o cache da lista de produtos, pois um novo produto foi adicionado
+      cache.del("lista_produtos");
+
       return novoProduto;
     } catch (error) {
       throw new Error("Erro ao criar produto");
@@ -59,6 +97,9 @@ class ProdutoService {
         throw new Error("Produto não encontrado");
       }
 
+      // Limpa o cache da lista de produtos, pois um produto foi atualizado
+      cache.del("lista_produtos");
+
       return produtoAtualizado;
     } catch (error) {
       throw new Error("Erro ao atualizar produto");
@@ -73,6 +114,9 @@ class ProdutoService {
       if (!produtoExcluido) {
         throw new Error("Produto não encontrado");
       }
+
+      // Limpa o cache da lista de produtos, pois um produto foi excluído
+      cache.del("lista_produtos");
 
       return { message: "Produto excluído com sucesso" };
     } catch (error) {
