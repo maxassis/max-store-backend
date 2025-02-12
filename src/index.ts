3c1e59d1322005ef8e4d712b8bcf177c79ffdca3
max <max.assis@chatguru.com.br>
2025-02-12 19:29:38 -0300

    feat: fazendo a validação do carrinho no back

--- src/index.ts
+++ src/index.ts
@@ -18,8 +18,8 @@ async function connectToDatabase() {
   try {
     const dbURI =
       process.env.NODE_ENV === "test"
-        ? "mongodb://localhost:27017/testdb" // Banco de dados para testes
-        : process.env.MONGODB_URI || ""; // Banco de dados de desenvolvimento
+        ? "mongodb://localhost:27017/testdb"
+        : process.env.MONGODB_URI || "";
 
     await mongoose.connect(dbURI);
     console.log(
@@ -27,7 +27,7 @@ async function connectToDatabase() {
     );
   } catch (error) {
     console.error("Erro ao conectar ao MongoDB:", error);
-    process.exit(1); // Encerra o processo em caso de erro
+    process.exit(1);
   }
 }
 
--- src/services/cart_service.ts
+++ src/services/cart_service.ts
@@ -1,17 +1,31 @@
-import Cart, { CartItem } from "../models/cart";
+import CartModel, { CartItem } from "../models/cart";
+import Produto from "../models/product";
+
 class CartService {
   async updateOrCreateCart(userId: string, items: CartItem[]) {
-    let cart = await Cart.findOne({ userId });
+    const productIds = items.map((item) => item._id);
+    const products = await Produto.find({ _id: { $in: productIds } });
+
+    for (const item of items) {
+      const product = products.find((p) => p._id.toString() === item._id);
+
+      if (!product) {
+        throw new Error("Produto não encontrado");
+      }
+
+      if (product.stock < item.qtdProduct) {
+        throw new Error("Estoque insuficiente");
+      }
+    }
+
+    let cart = await CartModel.findOne({ userId });
 
     if (!cart) {
-      const newCart = new Cart({ userId, items });
+      const newCart = new CartModel({ userId, items });
       return await newCart.save();
     }
 
-    cart.items = items;
-
-    cart.items = cart.items.filter((item) => item.qtdProduct > 0);
-
+    cart.items = items.filter((item) => item.qtdProduct > 0);
     return await cart.save();
   }
 }
